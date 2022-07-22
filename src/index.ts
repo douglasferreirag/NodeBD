cdf9e844ae28ee2d494085dca5bf6e5d37641455
douglasferreirag <douglas.ferreirag@gmail.com>
2022-07-21 22:48:30 -0300

    Configurando banco de dados

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, {Request, Response, NextFunction} from 'express';
+
+import statusRoute from './routes/status.route';
+
+import usersRoute from './routes/users.route';
+
+const app = express();
+
+//Configurações da aplicação.
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// Configurações de rotas.
+
+app.use(statusRoute);
+
+app.use(usersRoute);
+
+
+
+
+
+//Inicialização do servidor.
+
+app.listen(3000, () => {
+
+        console.log('Aplicação executando na porta 3000');
+
+})
+
